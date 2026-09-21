d4535c5ea1018445399e20550192d0d4ec2ec903
ValdirAcougueiro <BritoMachado462@protonmail.com>
2026-09-21 16:13:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// Transformar uma interface previamente estruturada e estática em uma
-// Single Page Application (SPA) dinâmica. Será necessário implementar
-// navegação fluida, criar um sistema de templatesJavaScript, estruturar
-// rotinas de validação de formulários com feedback adequado ao utilizador,
-// além de assegurar o armazenamento de dados através do localStorage.
-// Adicionalmente, você deverá garantir que o código JavaScript seja modularizado
-// e organizado por áreas de funcionalidade.
+// Transformar uma interface previamente estruturada e estática em uma Single
+// Page Application (SPA) dinâmica. Será necessário implementar navegação
+// fluida, criar um sistema de templatesJavaScript, estruturar rotinas de
+// validação de formulários com feedback adequado ao utilizador, além de
+// assegurar o armazenamento de dados através do localStorage. Adicionalmente,
+// você deverá garantir que o código JavaScript seja modularizado e organizado
+// por áreas de funcionalidade.
