ff7d63f288e191305c4167afffeeef8ab0a219ca
ValdirAcougueiro <BritoMachado462@protonmail.com>
2026-09-22 16:31:28 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,52 @@
 // assegurar o armazenamento de dados através do localStorage. Adicionalmente,
 // você deverá garantir que o código JavaScript seja modularizado e organizado
 // por áreas de funcionalidade.
-var test = document.getElementById('teste');
-console.log(test);
 
-document.getElementById('meuBotao').addEventListener('click', function() {
-    alert('Botão clicado!');
-  });
 
-document.write(test.innerHTML);
+//preventDefault()
+const formDefault = document.querySelector(".formtres");
+const respDefault = document.getElementById("inftres");
+
+formDefault.addEventListener('submit', executaDefault);
+
+function executaDefault(e) {
+  console.log('${e.type}');
+  console.log(respDefault.value);
+
+  e.preventDefault();
+};
+
+//template
+const butCad = document.querySelector('#button-cad');
+const cad = document.querySelector('#tpl-cad');
+const divcad = document.querySelector('.cadp');
+
+butCad.addEventListener('click', function(){
+  divcad.append(cad.content.cloneNode(true));
+});
+
+
+const tel = document.querySelector("#form-one");
+const wel = document.querySelector("#welcome");
+const alert = document.querySelector("#userset");
+
+tel.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const telnum = document.querySelector("#tel")
+
+  localStorage.setItem("tel", telnum.value)
+});
+
+//localStorage.setItem("tel","numero");
+//const tel = localStorage.getItem("tel");
+//console.log(tel);
+
+//var test = document.getElementById('teste');
+//console.log(test);
+//
+//document.getElementById('meuBotao').addEventListener('click', function() {
+//    alert('Botão clicado!');
+//  });
+
+//document.write(test.innerHTML);
